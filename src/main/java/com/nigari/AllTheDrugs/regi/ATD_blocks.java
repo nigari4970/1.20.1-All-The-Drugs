c7c6390f5fad396eea94f1e8aba381e4642856cf
nigari4970 <kairi63000@gmail.com>
2026-06-16 21:36:12 +0900

    コカの原木追加

--- src/main/java/com/nigari/AllTheDrugs/regi/ATD_blocks.java
+++ src/main/java/com/nigari/AllTheDrugs/regi/ATD_blocks.java
@@ -1,5 +1,6 @@
 package com.nigari.AllTheDrugs.regi;
 
+import com.nigari.AllTheDrugs.block.CocaLog;
 import com.nigari.AllTheDrugs.block.crusher;
 import com.nigari.AllTheDrugs.block.opium_casing;
 import com.nigari.AllTheDrugs.block.opiumium_block;
@@ -19,6 +20,7 @@ public class ATD_blocks {
         public static final RegistryObject<Block> OPIUMIUM_BLOCK = BLOCKS.register("opiumium_block", () -> new opiumium_block());
         public static final RegistryObject<Block> OPIUM_CASING = BLOCKS.register("opium_casing", () -> new opium_casing());
         public static final RegistryObject<Block> CRUSHER = BLOCKS.register("crusher", () -> new crusher());
+        public static final RegistryObject<Block> COCA_LOG = BLOCKS.register("coca_log", () -> new CocaLog());
     }
 
     public static class Block_Items {
@@ -27,5 +29,6 @@ public class ATD_blocks {
         public static final RegistryObject<Item> OPIUMIUM_BLOCK = BLOCK_ITEMS.register("opiumium_block", () -> new BlockItem(Blocks.OPIUMIUM_BLOCK.get(), new Item.Properties()));
         public static final RegistryObject<Item> OPIUM_CASING = BLOCK_ITEMS.register("opium_casing", () -> new BlockItem(Blocks.OPIUM_CASING.get(), new Item.Properties()));
         public static final RegistryObject<Item> CRUSHER = BLOCK_ITEMS.register("crusher", () -> new BlockItem(Blocks.CRUSHER.get(), new Item.Properties()));
+        public static final RegistryObject<Item> COCA_LOG = BLOCK_ITEMS.register("coca_log", () -> new BlockItem(Blocks.COCA_LOG.get(), new Item.Properties()));
     }
 }
