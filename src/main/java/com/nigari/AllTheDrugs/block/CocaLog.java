9beea7965bb43b8ef5d95e2ff72414d49e582076
nigari4970 <kairi63000@gmail.com>
2026-06-19 11:15:26 +0900

    コカの原木の向きが変わるように修正主義

--- src/main/java/com/nigari/AllTheDrugs/block/CocaLog.java
+++ src/main/java/com/nigari/AllTheDrugs/block/CocaLog.java
@@ -1,8 +1,9 @@
 package com.nigari.AllTheDrugs.block;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 
-public class CocaLog extends Block {
+public class CocaLog extends RotatedPillarBlock {
     public CocaLog() {
         super(Properties.of()
                 .strength(5.5F, 7.0F)
